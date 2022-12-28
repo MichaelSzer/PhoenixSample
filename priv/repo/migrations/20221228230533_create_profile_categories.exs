c4e3a2bda4d1a66e88e3fbf6874a0ddd0681ed2e
MichaelSzer <michaelszer@gmail.com>
2022-12-28 18:14:56 -0500

    added migration for profile_categories table.

--- priv/repo/migrations/20221228230533_create_profile_categories.exs
+++ priv/repo/migrations/20221228230533_create_profile_categories.exs
@@ -2,6 +2,13 @@ defmodule PhoenixSample.Repo.Migrations.CreateProfileCategories do
   use Ecto.Migration
 
   def change do
+    create table(:profile_categories, primary_key: false) do
+      add :profile_id, references(:profiles, on_delete: :delete_all)
+      add :category_id, references(:categories, on_delete: :delete_all)
+    end
 
+    create index(:profile_categories, [:profile_id])
+    create index(:profile_categories, [:category_id])
+    create unique_index(:profile_categories, [:profile_id, :category_id])
   end
 end
