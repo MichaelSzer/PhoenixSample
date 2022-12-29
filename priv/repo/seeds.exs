118f40db6bb8dc28e944928357f7d56a4dbb622c
MichaelSzer <michaelszer@gmail.com>
2022-12-28 19:14:56 -0500

    script to add default values to categories table.

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,6 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+for title <- ["Gamer", "Rock Fan", "Computers Geek", "Coffee Lover", "Soccer Fan", "College Student", "Highschool Student"] do
+  PhoenixSample.Manager.create_category(%{title: title})
+end
