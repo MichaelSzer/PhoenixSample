e108ea7b563897a179d548de790837aed2011af6
MichaelSzer <michaelszer@gmail.com>
2023-01-02 23:29:39 -0500

    Created functional component for form headers.

--- lib/phoenix_sample_web/views/page_view.ex
+++ lib/phoenix_sample_web/views/page_view.ex
@@ -1,3 +1,30 @@
 defmodule PhoenixSampleWeb.PageView do
   use PhoenixSampleWeb, :view
+
+  @doc """
+  Functional component for form section headers.
+
+  Required assigns:
+  - @title
+
+  Optional assigns:
+  - @classes
+
+
+  ## Example
+        <PhoenixSampleWeb.PageView.form_section title={"User Information"} classes={"text-red-500"} />
+  """
+  def form_section(assigns) do
+    classes = if assigns[:classes], do: assigns.classes, else: ""
+
+    assigns =
+      assigns
+      |> assign(:classes, classes)
+
+    ~H"""
+    <p class={"#{@classes} text-xl font-bold mt-6"}>
+      <%= @title %>
+    </p>
+    """
+  end
 end
